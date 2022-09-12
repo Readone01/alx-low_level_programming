2e6a14ac99f49658d132b5a3268ffd6aa9dd8d6e
Ridwan2003 <shotonwa2003@gmail.com>
2022-09-12 13:11:44 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,7 +8,7 @@
 
 int main(void)
 
-{
+ {
 int i, e, g;
 
 i = 48;
@@ -17,18 +17,18 @@ g = 48;
 
 while (e < 58)
 
-{
+ {
 i = 48;
 while (i < 58)
 
-{
+ {
 g = 48;
 while (g < 58)
 
-{
+ {
 if(e != i && e != g && i != g && e < i && i < g)
 
-{
+ { 
 putchar(e);
 putchar(i);
 putchar(g);
