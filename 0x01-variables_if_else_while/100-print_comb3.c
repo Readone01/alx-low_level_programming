51978999741eff8995ea769ccd6931ff476eb60d
Ridwan2003 <shotonwa2003@gmail.com>
2022-09-12 10:35:43 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,37 +9,37 @@
 int main(void)
 
 {
-int i, e;
-i = 48;
-e = 48;
+int i, e; 
+i = 48; 
+e = 48; 
 
 while (e < 58)
 
 {
-i = 48;
+i = 48; 
 while (i < 58)
 
 {
 if (e != i && e < i)
 
 {
-putchar(e);
-putchar(i);
+putchar(e); 
+putchar(i); 
 
 if (i == 57 && e == 56)
 
 {break;}
 
-putchar(',');
-putchar(' ');
+putchar(','); 
+putchar(' '); 
 }
 
-i++;
+i++; 
 }
 
-e++;
+e++; 
 }
 
-putchar('\n');
-return (0);
+putchar('\n'); 
+return (0); 
 }
