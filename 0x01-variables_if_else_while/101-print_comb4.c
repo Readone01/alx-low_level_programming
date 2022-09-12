8f208f71d048895a62d80a712bba6a9e6176f12c
Ridwan2003 <shotonwa2003@gmail.com>
2022-09-12 12:25:00 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,25 +16,26 @@ e = 48;
 g = 48;
 
 while (e < 58)
-	
+
 {
 i = 48;
 while (i < 58)
-		
+
 {
 g = 48;
 while (g < 58)
-			
+
 {
-if (e != i && e != g && i != g && e < i && i < g)
+if(e != i && e != g && i != g && e < i && i < g)
+
 {
 putchar(e);
 putchar(i);
 putchar(g);
 if (i == 56 && e == 55 && g == 57)
-					
+
 {
-break; 
+break;
 }
 
 putchar(',');
@@ -49,7 +50,7 @@ i++;
 
 e++;
 }
-	
+
 putchar('\n');
 return (0);
 }
