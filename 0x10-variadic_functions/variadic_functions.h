42b8922941a80bf14a792353494600db8fc354c8
Ridwan2003 <shotonwa2003@gmail.com>
2022-10-13 21:20:17 -0700

    Task 01

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#define _VARIADIC_FUNCTIONS_
+
+#endif
